41b05dd8c82091d0e5b3c7b116c6efba863d79e5
MiguelArmenta <89048570+MiguelArmenta@users.noreply.github.com>
2024-09-01 19:24:13 -0700

    step 21

--- index.js
+++ index.js
@@ -14,18 +14,22 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint'})
-}
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   
   if (error.name === 'CastError') {
     return response.status(400).send({error: 'malformatted id'})
+  } else if (error.name === 'ValidationError'){
+    return response.status(400).json({ error: error.message})
   }
 
   next(error)
+}
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint'})
 }
 
 app.get('/api/persons', (request, response) => {
@@ -61,7 +65,7 @@ app.post('/api/persons', (request, response, next) => {
 
 
     Person.find({ name: person.name}).then(findedPerson => {
-      if(findedPerson){
+      if(findedPerson.length > 0){
         return response.status(400).json({ 
           error: 'name must be unique'
         })
@@ -83,14 +87,17 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number} = request.body
 
   const person = {
     name: body.name,
     number: body.number
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true})
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query'})
   .then(updatedPerson => {
     response.json(updatedPerson)
   })
